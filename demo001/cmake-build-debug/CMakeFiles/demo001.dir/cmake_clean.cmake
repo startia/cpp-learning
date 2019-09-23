8bd60ce51d46910bb78986019c99994990e7c637
李新 <lixin@huodada.com>
2019-09-23 18:06:19 +0800

    first commit

---
+++ demo001/cmake-build-debug/CMakeFiles/demo001.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo001.dir/main.cpp.o"
+  "CMakeFiles/demo001.dir/demo001.cpp.o"
+  "CMakeFiles/demo001.dir/demo002.cpp.o"
+  "demo001.pdb"
+  "demo001.exe"
+  "libdemo001.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
